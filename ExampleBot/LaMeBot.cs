dd73a96d1911a9d6d1b3846b5ae3293a15cfb9da
Lars Meske <lars.meske@stud.uni-due.de>
2020-02-03 17:56:50 +0100

    Initial Commit for the bot

---
+++ ExampleBot/LaMeBot.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SC2API_CSharp;
+using SC2APIProtocol;
+
+namespace ExampleBot
+{
+    class LaMeBot : Bot
+    {
+        public void OnStart(ResponseGameInfo gameInfo, ResponseObservation observation, uint playerId)
+        { }
+    
+        public IEnumerable<SC2APIProtocol.Action> OnFrame(ResponseGameInfo gameInfo, ResponseObservation observation, uint playerId)
+        {
+            List<SC2APIProtocol.Action> actions = new List<SC2APIProtocol.Action>();
+
+            return actions;
+        }
+        
+        public void OnEnd(ResponseGameInfo gameInfo, ResponseObservation observation, uint playerId, Result result)
+        { }
+
+        public IEnumerable<SC2APIProtocol.Action> OnFrame(ResponseObservation observation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnEnd(ResponseObservation observation, Result result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnStart(ResponseGameInfo gameInfo, ResponseData data, ResponseObservation observation, uint playerId, string opponentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SC2API-CSharp/Bot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using SC2APIProtocol;
+
+namespace SC2API_CSharp
+{
+    public interface Bot
+    {
+        IEnumerable<SC2APIProtocol.Action> OnFrame(ResponseObservation observation);
+        void OnEnd(ResponseObservation observation, Result result);
+        void OnStart(ResponseGameInfo gameInfo, ResponseData data, ResponseObservation observation, uint playerId, String opponentId);
+    }
+}
