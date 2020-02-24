1a7185ee830370f1516cd6a5771d0b8a7ac728bd
Lars Meske <lars.meske@stud.uni-due.de>
2020-02-24 17:36:12 +0100

    added nugets for machine learning

--- ExampleBot/LaMeBot.cs
+++ ExampleBot/LaMeBot.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using SC2API_CSharp;
 using SC2APIProtocol;
 using Action = SC2APIProtocol.Action;
+using SharpCV;
+using NumSharp;
+using Tensorflow;
 
 
 namespace LaMeBotNS
@@ -111,104 +114,6 @@ namespace LaMeBotNS
 
         public IEnumerable<SC2APIProtocol.Action> OnFrame(ResponseObservation observation)
         {
-          //  Observation = observation;
-          //  Stopwatch stopWatch = Stopwatch.StartNew();
-          // actions = new List<Action>();
-
-
-            //Controller.OpenFrame();
-
-            //if (Controller.frame == 0)
-            //{
-            //    Logger.Info("RaxBot");
-            //    Logger.Info("--------------------------------------");
-            //    Logger.Info("Map: {0}", Controller.gameInfo.MapName);
-            //    Logger.Info("--------------------------------------");
-            //}
-
-            //if (Controller.frame == Controller.SecsToFrames(1))
-            //    Controller.Chat("gl hf");
-
-            //var structures = Controller.GetUnits(Units.Structures);
-            //if (structures.Count == 1)
-            //{
-            //    //last building                
-            //    if (structures[0].integrity < 0.4) //being attacked or burning down                 
-            //        if (!Controller.chatLog.Contains("gg"))
-            //            Controller.Chat("gg");
-            //}
-
-            //var resourceCenters = Controller.GetUnits(Units.ResourceCenters);
-            //foreach (var rc in resourceCenters)
-            //{
-            //    if (Controller.CanConstruct(Units.SCV))
-            //        rc.Train(Units.SCV);
-            //}
-           // return actions;
-
-            Controller.OpenFrame();
-
-            if (Controller.frame == 0)
-            {
-                Logger.Info("LaMeBot");
-                Logger.Info("--------------------------------------");
-                Logger.Info("Map: {0}", Controller.gameInfo.MapName);
-                Logger.Info("--------------------------------------");
-            }
-
-            if (Controller.frame == Controller.SecsToFrames(1))
-                Controller.Chat("gl hf");
-
-            var structures = Controller.GetUnits(Units.Structures);
-            if (structures.Count == 1)
-            {
-                //last building                
-                if (structures[0].integrity < 0.4) //being attacked or burning down                 
-                    if (!Controller.chatLog.Contains("gg"))
-                        Controller.Chat("gg");
-            }
-
-            var resourceCenters = Controller.GetUnits(Units.ResourceCenters);
-            foreach (var rc in resourceCenters)
-            {
-                if (Controller.CanConstruct(Units.SCV))
-                    rc.Train(Units.SCV);
-            }
-
-
-            //keep on buildings depots if supply is tight
-            if (Controller.maxSupply - Controller.currentSupply <= 5)
-                if (Controller.CanConstruct(Units.SUPPLY_DEPOT))
-                    if (Controller.GetPendingCount(Units.SUPPLY_DEPOT) == 0)
-                        Controller.Construct(Units.SUPPLY_DEPOT);
-
-
-            //distribute workers optimally every 10 frames
-            if (Controller.frame % 10 == 0)
-                Controller.DistributeWorkers();
-
-
-
-            //build up to 4 barracks at once
-            if (Controller.CanConstruct(Units.BARRACKS))
-                if (Controller.GetTotalCount(Units.BARRACKS) < 4)
-                    Controller.Construct(Units.BARRACKS);
-
-            //train marine
-            foreach (var barracks in Controller.GetUnits(Units.BARRACKS, onlyCompleted: true))
-            {
-                if (Controller.CanConstruct(Units.MARINE))
-                    barracks.Train(Units.MARINE);
-            }
-
-            //attack when we have enough units
-            var army = Controller.GetUnits(Units.ArmyUnits);
-            if (army.Count > 20)
-            {
-                if (Controller.enemyLocations.Count > 0)
-                    Controller.Attack(army, Controller.enemyLocations[0]);
-            }
-
             return Controller.CloseFrame();
         }
 
@@ -231,36 +136,9 @@ namespace LaMeBotNS
             Controller.gameInfo = gameInfo;
             Controller.gameData = data;
             Controller.obs = observation;
-            // Observation = observation;
-            // MyRace = GameInfo.PlayerInfo[(int)Observation.Observation.PlayerCommon.PlayerId - 1].RaceActual;
+            
             DebugUtil.WriteLine("MyRace: ");
-            //GameInfo = gameInfo;
-            //PlayerId = playerId;
-            //Data = data;
-            //UnitTypes.LoadData(data);
-
-            //GameVersion = pingResponse.GameVersion;
-            //OldMapData = SC2Util.IsVersionBefore("4.9.3");
-            //DebugUtil.WriteLine("Game version: " + pingResponse.GameVersion);
-
-            //OpponentID = opponentID;
-
-            //MyRace = GameInfo.PlayerInfo[(int)Observation.Observation.PlayerCommon.PlayerId - 1].RaceActual;
-            //EnemyRace = GameInfo.PlayerInfo[2 - (int)Observation.Observation.PlayerCommon.PlayerId].RaceActual;
-            //DebugUtil.WriteLine("MyRace: " + MyRace);
-            //DebugUtil.WriteLine("EnemyRace: " + EnemyRace);
-            //DebugUtil.WriteLine("Game started on map: " + GameInfo.MapName);
-
-            //FileUtil.Log("Game started on map: " + GameInfo.MapName);
-            //FileUtil.Log("Enemy race: " + EnemyRace);
-
-            //MapAnalyzer.Analyze(this);
-            //TargetManager.OnStart(this);
-            //BaseManager.OnStart(this);
-
-            //Build = DetermineBuild();
-            //Build.InitializeTasks();
-            //Build.OnStart(this);
+           
         }
     }
 }
